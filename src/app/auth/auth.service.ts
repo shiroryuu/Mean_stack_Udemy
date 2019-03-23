3b2b5869c3e33646c06f0541894dbaf218fd04e6
Arvind Hariharan Nair <arvdev96@gmail.com>
2019-03-23 23:59:29 +0530

    🔧 Fix URL

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -5,7 +5,7 @@ import { Subject } from 'rxjs';
 import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment';
 
-const BACKEND_URL = environment.apiUrl+'user';
+const BACKEND_URL = environment.apiUrl+'/user';
 @Injectable({ providedIn: 'root' })
 export class AuthService {
   private isAuthenticated = false;
